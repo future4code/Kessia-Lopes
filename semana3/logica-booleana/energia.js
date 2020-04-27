92d71231ebc0fe32806cbb1e87ef070b8009bc26
Kessialopes <lopes.chipmunk@gmail.com>
2020-04-27 16:20:43 -0300

    exercicios js 1

---
+++ semana3/logica-booleana/energia.js
@@ -0,0 +1,7 @@
+const quilowattHora = 0.005
+let consumoCasa = 280
+const desconto = 15/100
+let calculoCasa = consumoCasa*quilowattHora
+let calculoCasaDesconto = consumoCasa*quilowattHora*desconto
+console.log(calculoCasa)
+console.log(calculoCasaDesconto)
