a9d66f82ab8b6bac5eee1d42deaa51ab0ef64ebe
Kessialopes <lopes.chipmunk@gmail.com>
2020-04-28 16:56:57 -0300

    Exercicios Js corretos

--- semana3/condicional/index.js
+++ semana3/condicional/index.js
@@ -97,11 +97,17 @@ if(numero1>numero2 && numero2>numero3){
 else if (numero1<numero2&& numero2<numero3){
     console.log(numero3,numero2,numero1)
 
-}else if (numero2>numero1&& numero1>numero3){
+}else if (numero1>numero3&& numero3>numero2){
+    console.log(numero1,numero3,numero2)
+
+}else if(numero2>numero1&& numero1>numero3){
     console.log(numero2,numero1,numero3)
 
+}else if(numero2>numero3&& numero3>numero1){
+    console.log(numero2,numero3,numero1)
+
 }else if(numero3>numero1&& numero1>numero2){
-    console.log(numero3,numero1,numero2)
+    console.log(numero3,numero1,numero2)    
 
 }else if(numero1===numero2&&numero2===numero3){
     console.log("Insira pelo menos um número diferente")
