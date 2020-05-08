c066353923aeb718f805703b0d2a7336d8315ed4
Kessialopes <lopes.chipmunk@gmail.com>
2020-05-08 12:53:20 -0300

    segundo exercicio de interpretação

--- semana4/lista-de-exercicios/projeto4.js
+++ semana4/lista-de-exercicios/projeto4.js
@@ -1,3 +1,6 @@
 /*Exercísios de interpretação de código:
 1-R: É uma função para converter os 100 dólares passado por parametro
 para a cotação informada pelo usuário. Na cotação atual o console mostraria: "R$ 575"
+
+2-R: É uma função que seleciona os investimentos e retorna o valor passado por parametro multiplicado pelo investimento selecionado.
+Para a variavel novoMontante o console mostra o valor de 165. E para a variável segundoMontante, é retornado um alert com a mensagem "TIPO DE INVESTIMENTO INFORMADO INCORRETO"
