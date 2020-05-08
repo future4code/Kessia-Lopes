5ef9c8051abe493eb7c5287cfee6a1760e0f9546
Kessialopes <lopes.chipmunk@gmail.com>
2020-05-08 12:47:24 -0300

    Exercicio 1 de interpretação

---
+++ semana4/lista-de-exercicios/projeto4.js
@@ -0,0 +1,3 @@
+/*Exercísios de interpretação de código:
+1-R: É uma função para converter os 100 dólares passado por parametro
+para a cotação informada pelo usuário. Na cotação atual o console mostraria: "R$ 575"
