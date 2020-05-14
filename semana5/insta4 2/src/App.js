40b4c8744eef9cc4a6d9388c27d58e17d41fbbdc
Kessialopes <lopes.chipmunk@gmail.com>
2020-05-14 16:22:50 -0300

    iniciando as listas

--- semana5/insta4 2/src/App.js
+++ semana5/insta4 2/src/App.js
@@ -9,24 +9,35 @@ import postagemDois from './img/image2.jpg'
 import postagemTres from './img/image6.jpg'
 
 class App extends React.Component {
+  state={
+    postagens=[
+      {
+          nomeUsuario:'Kessia',
+          fotoUsuario:perfilUm,
+          fotoPost:postagemUm
+    },
+    {
+          nomeUsuario:'Angelica',
+          fotoUsuario:perfilDois,
+          fotoPost:postagemDois
+    },
+    {
+          nomeUsuario:'Lucas',
+          fotoUsuario:perfilTres,
+          fotoPost:postagemTres
+
+    }
+      
+    ]
+
+  };
   render() {
+    const listaDePosts= this.state.postagens.map((postagem)=>{
+
+    });
     return (
       <div className={'app-container'}>
-        <Post
-          nomeUsuario={'Kessia'}
-          fotoUsuario={perfilUm}
-          fotoPost={postagemUm}
-        />
-        <Post
-          nomeUsuario={'Angelica'}
-          fotoUsuario={perfilDois}
-          fotoPost={postagemDois}
-        />
-        <Post
-          nomeUsuario={'Lucas'}
-          fotoUsuario={perfilTres}
-          fotoPost={postagemTres}
-        />
+        <div>{listaDePosts}</div>
       </div>
     );
   }
