e523f7dec13b31b35f2f3190e05c157893b0aa85
Kessialopes <lopes.chipmunk@gmail.com>
2020-07-28 22:26:22 -0300

    Correções no ex2 e tentando ex3

--- BackEnd/FirstScript2/src/ex2.ts
+++ BackEnd/FirstScript2/src/ex2.ts
@@ -1,17 +1,21 @@
 function equacao(){
-    const count: string = process.argv[2]
+    const operation: string = process.argv[2]
     const num1: number = Number(process.argv[3])
     const num2: number = Number(process.argv[4])
     
-    switch (count){
+    switch (operation){
         case 'add' :
             return  num1 + num2
+        break;
         case 'sub':
             return  num1 - num2 
+        break;
         case 'div':
             return  num1 / num2
+        break;
         case 'mult':
-            return  num1 * num2     
+            return  num1 * num2   
+        
     }
     
 }
