48e696ffe2fd2197c24733a8f281d5d5037e55b4
Kessialopes <lopes.chipmunk@gmail.com>
2020-08-04 17:07:11 -0300

    Exercicios de Classe segunda aula

---
+++ BackEnd/Classes-aula2/src/user.ts
@@ -0,0 +1,29 @@
+export default class User{
+    private id: string;
+    private email: string;
+    private name: string;
+    private password: string;
+  
+  constructor(id: string, email: string, name: string, password: string){
+    console.log("Chamando o construtor da classe User")
+    this.id = id
+    this.email = email
+    this.name = name
+    this.password = password
+  }
+  public getId(): string{
+    return this.id
+  }
+  public getEmail(): string{
+    return this.email
+  
+  }
+  public getName(): string{
+    return this.name
+  
+  }
+  public introducerYourself(): string{
+      return `Olá, sou a (o) ${this.name}. Bom dia!`
+  }
+  
+  }
